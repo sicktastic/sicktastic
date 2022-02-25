f2d84befbd34bd313160e28dab7eef05b0121aea
Anthony Lee <antwonlee@gmail.com>
2022-02-25 11:45:31 -0600

    feat: Add sicktastic logo

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,35 @@
+// NextJS
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import Link from "next/link";
+
+// Sicktastic
+import styles from "../styles/Home.module.css";
+import SicktasticLogo from "../public/sicktastic.svg";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Sicktastic</title>
+        <meta
+          name="description"
+          content="A combination of the words sick and fantastic."
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Sicktastic</h1>
+        <Link href="https://anthonylee.io">
+          <a>
+            <Image
+              src={SicktasticLogo}
+              alt="Sicktastic Logo"
+              width={500}
+              height={200}
+            />
+          </a>
+        </Link>
         <p className={styles.textGray}>
           A combination of the words sick and fantastic.
         </p>
