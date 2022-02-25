e2b7f5be14b2b290c34b04ee374de2cbc5bfc8ff
Anthony Lee <antwonlee@gmail.com>
2022-02-25 12:09:45 -0600

    feat: Add simple redirect

--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,44 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
+// React
+import redirect from "nextjs-redirect";
+
 // Sicktastic
 import styles from "../styles/Home.module.css";
 import SicktasticLogo from "../public/sicktastic.svg";
 
+const Redirect = redirect("https://anthonylee.io");
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Sicktastic</title>
-        <meta
-          name="description"
-          content="A combination of the words sick and fantastic."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Redirect>
+      <div className={styles.container}>
+        <Head>
+          <title>Sicktastic</title>
+          <meta
+            name="description"
+            content="A combination of the words sick and fantastic."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <Link href="https://anthonylee.io">
-          <a>
-            <Image
-              src={SicktasticLogo}
-              alt="Sicktastic Logo"
-              width={500}
-              height={200}
-            />
-          </a>
-        </Link>
-        <p className={styles.textGray}>
-          A combination of the words sick and fantastic.
-        </p>
-      </main>
-    </div>
+        <main className={styles.main}>
+          <Link href="https://anthonylee.io">
+            <a>
+              <Image
+                src={SicktasticLogo}
+                alt="Sicktastic Logo"
+                width={500}
+                height={200}
+              />
+            </a>
+          </Link>
+          <p className={styles.textGray}>
+            A combination of the words sick and fantastic.
+          </p>
+        </main>
+      </div>
+    </Redirect>
   );
 }
